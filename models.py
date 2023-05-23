86cd41e5cb00e3acf0855b3069fdd5f77c699375
Cal <72684566+Jimdangle@users.noreply.github.com>
2023-05-23 13:27:39 -0700

    Added DB tables for pixels and user click times, need to add functionality to them

--- models.py
+++ models.py
@@ -2,6 +2,8 @@
 This file defines the database models
 """
 
+
+
 import datetime
 from .common import db, Field, auth
 from pydal.validators import *
@@ -13,11 +15,26 @@ def get_user_email():
 def get_time():
     return datetime.datetime.utcnow()
 
+def get_time_timestamp():
+    return get_time().timestamp()
 
 ### Define your table below
 #
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
+
+db.define_table('Board',
+                Field('pos_x','integer',required=True),
+                Field('pos_y', 'integer', required=True),
+                Field('color', 'string', required=True, requires=IS_IN_SET(["black","yellow","blue","green","red"]))
+                )
+
+db.commit()
+
+db.define_table('UClick',
+                Field('uid', 'references auth_user', required=True),
+                Field('last_click', 'integer', default=get_time_timestamp())
+                )
 
 db.commit()
