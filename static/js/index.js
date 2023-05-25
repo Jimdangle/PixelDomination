22b695de8320902b7d92904333583b328ad3c2d7
Will Hord <whord@ucsc.edu>
2023-05-24 18:55:01 -0700

    color selector is now collapsable

--- static/js/index.js
+++ static/js/index.js
@@ -18,6 +18,7 @@ let init = (app) => {
     isDrawing: false,
     gridSize: 20,
     selectedColor: "black",
+    colorSelectorShown: false,
   };
 
   app.enumerate = (a) => {
@@ -77,6 +78,10 @@ let init = (app) => {
     app.data.selectedColor = color;
   };
 
+  app.toggleColorSelector = () => {
+    app.data.colorSelectorShown = !app.data.colorSelectorShown;
+  };
+
   app.get_pixels = function() {
     console.log("Getting pixels")
     axios({
@@ -99,6 +104,7 @@ let init = (app) => {
     startDrawing: app.startDrawing,
     stopDrawing: app.stopDrawing,
     get_pixels: app.get_pixels,
+    toggleColorSelector: app.toggleColorSelector,
   };
 
   // This creates the Vue instance.
