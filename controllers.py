7d80064dc765ecc582c4833ebe5cacb1edb763e6
Cal <72684566+Jimdangle@users.noreply.github.com>
2023-05-31 18:32:56 -0700

    Added player stats, currently tracking clicks

--- controllers.py
+++ controllers.py
@@ -37,6 +37,8 @@ url_signer = URLSigner(session)
 @action.uses('index.html', db, auth.user, url_signer)
 def index():
     print(get_time_timestamp())
+    
+
     return dict(
         # COMPLETE: return here any signed URLs you need.
         my_callback_url = URL('my_callback', signer=url_signer),
@@ -48,9 +50,17 @@ def index():
 @action.uses(session, db, auth.user, url_signer.verify())
 def draw_url():
     user = get_user_id()
+    click_time = int(request.params.get('click_time'))
     x = int(request.params.get('y'))
     y = int(request.params.get('x'))
     color = request.params.get('color')
+
+    rows = db(db.Ply_Stats.user == user).select()
+    if len(rows) == 0: #first time a user has clicked
+        db.Ply_Stats.insert(user=user, last_click = click_time) #insert into stats table
+    
+
+
 
     if color == "init":
         #don't do anything
@@ -59,6 +69,7 @@ def draw_url():
         print(f'Place pixel at {x},{y}, color {color}')
         db((db.Board.pos_x==x) & (db.Board.pos_y==y)).delete()
         id = db.Board.insert(uid = user, pos_x = x, pos_y = y, color = color)
+        db(db.Ply_Stats.user==user).update(total_clicks=db.Ply_Stats.total_clicks+1) #update clicks
     
     pixels = db(db.Board.color != None).select()
     return dict(pixels=pixels)
@@ -71,4 +82,3 @@ def get_pixesl():
     return dict(
         pixels = pixels,
     )
-
