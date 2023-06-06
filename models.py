5cb7e10157bbe44495052d2c6d2f90e234d93ffc
Cal <72684566+Jimdangle@users.noreply.github.com>
2023-06-06 10:59:05 -0700

    Meant to update models, did the server browser skeleton instead. Needs some work on display but functionally should be cool, had some issues with the timestamping, ideally players should only set the time the game should be alive and we crunch the numbers and come up with a time of death for the game

--- models.py
+++ models.py
@@ -2,11 +2,16 @@
 This file defines the database models
 """
 
+ADJS = ["dead","hairless","sadistic","metal","wild","domesticated","abnormal","medicated","cocky","massive","hilarious","bearded","slimy","self-centered","talking","naked","angry"]
+
+NOUNS = ['blood rage', 'idiot', 'toaster', 'legend', 'death wish', 'therapy', 'psychic', 'knife', 'sandwich', 'hunting ground', 'lettuce', 'kitty', 'french chef', 'antidepressant drug', 'corn cake', 'president of the night', 'candlestick maker', 'coffee pot', 'brethren', 'national security agency', 'tank', 'useless brakes', 'international law enforcement agency', 'sound barrier', 'private investor', 'main people', 'stock car', 'elastic band', 'whole blood', 'telephone', 'mad cow disease']
+
 
 
 import datetime
 from .common import db, Field, auth
 from pydal.validators import *
+import random
 
 def get_user_id():
     return auth.current_user.get('id') if auth.current_user else None
@@ -20,29 +25,49 @@ def get_time():
 def get_time_timestamp():
     return get_time().timestamp()
 
+def gen_rand_name():
+    random.seed(get_time_timestamp())
+    adj = random.randint(1,len(ADJS))
+    noun = random.randint(1,len(NOUNS))
+    name = f'{ADJS[adj]} {NOUNS[noun]}'
+    print(name)
+    return name
+
 ### Define your table below
 #
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
 
+
+
+db.define_table('Games',
+                Field('name', 'string', default=gen_rand_name()),
+                Field('x_size', 'integer', required=True, requires=IS_INT_IN_RANGE(20,200,error_message='pick between 20,200')),
+                Field('y_size', 'integer', required=True, requires=IS_INT_IN_RANGE(20,200,error_message='pick between 20,200')),
+                Field('time_started','integer',required=True, default=get_time_timestamp()),
+                Field('move_interval','integer',required=True),
+                Field('live_time', 'integer', required=True, requires=IS_IN_SET([12,24,48,72]))
+                )
+
+db.Games.time_started.writable  = False
+db.Games.time_started.readable  = False
+
+
+
+db.commit()
+
 db.define_table('Board',
                 Field('uid','integer',required=True),
                 Field('pos_x','integer',required=True),
                 Field('pos_y', 'integer', required=True),
                 Field('color', 'string', required=True,),
-                Field('game_id', 'integer', required=True,)
+                Field('game_id', 'references Games', required=True,)
                 )
 
 db.commit()
 
-db.define_table('Games',
-                Field('game_id','integer',required=True),
-                Field('time_started','integer',required=True),
-                Field('move_interval','integer',required=True)
-                )
 
-db.commit()
 
 db.define_table('UClick',
                 Field('uid', 'references auth_user', required=True),
@@ -60,3 +85,4 @@ db.define_table('Ply_Stats',
 
 
 db.commit()
+
